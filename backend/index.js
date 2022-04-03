34b29f09d2d3bda750f1bb6b8e946598aefcc02e
Luiz Calaça <luizcalaca@Luizs-MacBook-Air.local>
2022-04-03 00:33:01 -0300

    refactor: changing the localhost port

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use('/user', userRoutes)
 mongoose.connect(`mongodb://127.0.0.1:27017/user`)
 .then(() => {
     console.log('Connection OK')
-    app.listen(3000)
+    app.listen(3333)
 })
 .catch((err) => console.log(err))
 
